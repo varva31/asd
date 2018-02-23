c35c21df904bf3be22949396f77392b4a975fb7d
varva31 <36750254+varva31@users.noreply.github.com>
2018-02-22 19:10:31 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <!-- generator meta tag -->
+    <!-- leave this for stats and Silex version check -->
+    <meta name="generator" content="Silex v2.2.7">
+    <!-- End of generator meta tag -->
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/jquery.js" data-silex-static=""></script>
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/jquery-ui.js" data-silex-static=""></script>
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/pageable.js" data-silex-static=""></script>
+    <script type="text/javascript" src="//editor.silex.me/static/2.7/front-end.js" data-silex-static=""></script>
+    <link rel="stylesheet" href="//editor.silex.me/static/2.7/normalize.css" data-silex-static="">
+    <link rel="stylesheet" href="//editor.silex.me/static/2.7/front-end.css" data-silex-static="">
+
+    <style type="text/css" class="silex-style"></style>
+    <script type="text/javascript" class="silex-script"></script>
+    <style class="silex-inline-styles" type="text/css">
+        .body-initial {
+            background-color: rgba(255, 255, 255, 1);
+        }
+        
+        .silex-id-1478366444112-1 {
+            top: 100px;
+            left: NaNpx;
+            background-color: rgba(0, 0, 0, 1);
+            min-width: 1200px;
+        }
+        
+        .silex-id-1478366444112-0 {
+            min-height: 185px;
+            background-color: transparent;
+            width: 1200px;
+            top: 15px;
+            left: 203px;
+            background-image: url('unnamed.png');
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            background-position: top center;
+        }
+        
+        .silex-id-1474394621033-3 {
+            top: 5px;
+            left: 0px;
+            background-color: rgba(0, 0, 0, 1);
+        }
+        
+        .silex-id-1474394621032-2 {
+            min-height: 600px;
+            background-color: transparent;
+            width: 1200px;
+        }
+        
+        .silex-id-1519347889643-0 {
+            width: 276px;
+            min-height: 117px;
+            top: 6px;
+            left: 461px;
+        }
+        
+        .silex-id-1519347978367-1 {
+            width: 400px;
+            height: 400px;
+            background-color: transparent;
+            top: 147px;
+            left: 414px;
+        }
+		
+		body {background-color: black;}
+    </style>
+    <title></title>
+    <script type="text/javascript" class="silex-json-styles">
+        [{"desktop":{"body-initial":{"background-color":"rgba(255,255,255,1)"},"silex-id-1474394621033-3":{"top":"5px","left":"0px","background-color":"rgba(0,0,0,1)"},"silex-id-1474394621032-2":{"min-height":"600px","background-color":"transparent","width":"1200px"},"silex-id-1474394605264-1":{"min-height":"100px","width":"100px","top":"100px","left":"100px"},"silex-id-1474394605263-0":{"min-height":"100px","background-color":"transparent"},"silex-id-1442914737143-3":{"min-height":"53px","width":"349px","top":"29px","left":"430px"},"silex-id-1478366444112-0":{"min-height":"185px","background-color":"transparent","width":"1200px","top":"15px","left":"203px","background-image":"url('unnamed.png')","background-attachment":"fixed","background-repeat":"no-repeat","background-position":"top center"},"silex-id-1478366444112-1":{"top":"100px","left":"NaNpx","background-color":"rgba(0,0,0,1)","min-width":"1200px"},"silex-id-1478366450713-2":{"min-height":"100px","background-color":"transparent","width":"1200px"},"silex-id-1519347889643-0":{"width":"276px","min-height":"117px","top":"6px","left":"461px"},"silex-id-1519347978367-1":{"width":"400px","height":"400px","background-color":"transparent","top":"147px","left":"414px"}},"mobile":{},"componentData":{"silex-id-1519347978367-1":{"name":"form1","templateName":"form","submitButtonLabel":"Look for Love","buttonTextColor":"#ff80ff","buttonBackgroundColor":"#000000","placeholder1":"IGN","label2":"The class your looking for","placeholder2":"Ninja, Witch, Kuno etc.....","label3":"Bio","placeholder3":"Fill out some info about your self.","sendTo":"varvakid123@gmail.com"}}}]
+    </script>
+
+
+
+
+
+    <meta name="website-width" content="1200">
+    <style type="text/css" class="silex-style-settings">
+        .website-width {
+            width: 1200px;
+        }
+    </style>
+
+
+
+
+
+
+
+
+    <style id="current-page-style">
+        .page-page-1 {
+            display: inherit;
+        }
+    </style>
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css" class="silex-custom-font">
+    <!-- Silex HEAD tag do not remove -->
+    <!-- End of Silex HEAD tag do not remove -->
+</head>
+
+<body data-silex-id="body-initial" class="body-initial silex-runtime" data-silex-type="container">
+    <div class="silex-pages">
+        <div data-silex-type="image" class="editable-style silex-id-hamburger-menu image-element menu-button prevent-draggable prevent-resizable" data-silex-id="silex-id-hamburger-menu">
+            <img alt="open mobile menu" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAoCAYAAAC8cqlMAAAB5UlEQVR42u2ZPWvCUBSGM+UfFO3e3dLu/QN+oKj4EUXRQREEJYgoiCAu6qCogyDo7iJ21UUaKqEgCC4iiEMGB3UVgsnpTWj/wfVK2nvhGbKEPJyT996TMMzPGg6H5mKx+GgkyuWyeTQasb8OTDwef4tEIp8+n+/LYIjJZJJHQiwzm81M6OLDarWqNpsNjEYgEJCy2ewT0+/3TagighElNFAnQT6ft1ARKkJFqMgfFvF4PFpuEwFtdrcRqVarIIoirFYrIiyXSxiPx+B2u/GJBINBuF6vQHopigK1Wg2fCMdxcLlciIuoqgqNRgOfiN1uB3Qwg91uB8fjkQiHwwEmkwne1qLxS0VuKKJle71eh06nQ4Rmswk8z+MVcTqdsNlsQJZlotF7Pp8hk8ngE4lGo/qN7xG/vV4Pb0Xm8zlxkdPpBGgMx/+yJxIJKBQKRMjlcuBwOGhq/Q+RcDgMsViMCFrAuFwu/CKDwQC22y1IkkSE/X4PgiDo+xc2Ea0S9zjGa/Hb7Xbxifj9fj0K7yFSqVTwtlY6nYbpdKpPiSRYLBbQarX0PYymFhWhIlSEilCRW6GdyUqlkoVZr9dsKpXiOY6TtW+7BkNBz/7ebrcf9L+6Xq+XRYPMMyrTq5EIhUIvaAjTJb4BBNQ2yhnth0wAAAAASUVORK5CYII="
+                class="silex-element-content">
+        </div>
+
+        <a id="page-page-1" data-silex-type="page" class="page-element page-link-active">Page 1</a></div>
+
+
+
+
+
+
+
+
+
+    <div data-silex-type="container" class="prevent-draggable container-element editable-style silex-id-1478366444112-1 section-element" data-silex-id="silex-id-1478366444112-1" style="">
+        <div data-silex-type="container" class="editable-style silex-element-content silex-id-1478366444112-0 silex-container-content container-element prevent-draggable website-width" data-silex-id="silex-id-1478366444112-0"></div>
+    </div>
+    <div data-silex-type="container" class="prevent-draggable container-element editable-style silex-id-1474394621033-3 section-element" data-silex-id="silex-id-1474394621033-3" style="">
+        <div data-silex-type="container" class="editable-style silex-element-content silex-id-1474394621032-2 silex-container-content container-element prevent-draggable website-width" data-silex-id="silex-id-1474394621032-2">
+            <div data-silex-type="text" class="editable-style silex-id-1519347889643-0 text-element" data-silex-id="silex-id-1519347889643-0" style="">
+                <div class="silex-element-content normal">
+                    <div style="text-align: center;">
+                        <font size="7" face="Roboto" color="#ff00ff">Relic Speed Dating</font>
+                    </div>
+                    <div style="text-align: center;">
+                        <font face="Roboto" color="#ff00ff" size="2">(No Tamers!)</font>
+                    </div>
+                </div>
+            </div>
+            <div data-silex-type="html" class="editable-style silex-id-1519347978367-1 html-element silex-component silex-component-form silex-use-height-not-minheight" data-silex-id="silex-id-1519347978367-1" style="">
+                <div class="silex-element-content">
+                    <form id="id_1519348174619_772" action="https://formspree.io/varvakid123@gmail.com" method="POST">
+
+                        <div>
+                            <label for="field1">Name</label>
+                            <input type="text" required="" id="field1" name="field1" placeholder="IGN">
+                        </div>
+
+
+                        <div>
+                            <label for="field2">The class your looking for</label>
+                            <input type="text" required="" id="field2" name="field2" placeholder="Ninja, Witch, Kuno etc.....">
+                        </div>
+
+
+                        <div class="fill-vertical">
+                            <label for="field3">Bio</label>
+                            <textarea required="" id="field3" name="field3" placeholder="Fill out some info about your self."></textarea>
+                        </div>
+
+                        <form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+                        <input type="submit" value="Look for Love">
+                    </form>
+                    <style>
+                        #id_1519348174619_772 input[type=text],
+                        select {
+                            width: 100%;
+                            padding: 12px 20px;
+                            margin: 8px 0;
+                            display: inline-block;
+                            border: 1px solid #ccc;
+                            border-radius: 3px;
+                            box-sizing: border-box;
+                        }
+                        
+                        #id_1519348174619_772 input[type=submit] {
+                            width: 100%;
+                            background-color: #000000;
+                            color: #ff80ff;
+                            padding: 14px 20px;
+                            margin: 8px 0;
+                            border: 1px solid #4CAF50;
+                            border-radius: 3px;
+                            cursor: pointer;
+                        }
+                        
+                        #id_1519348174619_772 label {
+                            color: #000000;
+                        }
+                        /* *************************** */
+                        /* resize the 3rd field
+   and fill the vertical space */
+                        
+                        #id_1519348174619_772 .fill-vertical {
+                            flex: 1 1 auto;
+                            display: flex;
+                            flex-direction: column;
+                        }
+                        
+                        #id_1519348174619_772 .fill-vertical textarea {
+                            flex: 1 1 auto;
+                            width: 100%;
+                            height: 150px;
+                            padding: 12px 20px;
+                            box-sizing: border-box;
+                            border: 2px solid #ccc;
+                            border-radius: 3px;
+                            background-color: #f8f8f8;
+                            font-size: 16px;
+                            resize: none;
+                        }
+                        /* ************************ */
+                        
+                        #id_1519348174619_772 {
+                            border-radius: 3px;
+                            display: flex;
+                            flex-direction: column;
+                            justify-content: space-between;
+                            height: 100%;
+                        }
+                    </style>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ upload.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+	// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+?>
+	
+</body>
+</html>
